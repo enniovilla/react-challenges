b1c1e4c40854deed7172db5b489426b845b2984d
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-25 14:54:43 +0000

    Create NavBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import StatefulGreeting from './components/StatefulGreeting';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
       <StatefulGreeting />
+      <NavBarSimple />
     </div>
   );
 }
