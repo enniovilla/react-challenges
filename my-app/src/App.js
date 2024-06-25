2096649fe0630713503b7e244b2d38b78bcd74b4
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-25 14:36:06 +0000

    Create StatefulGreeting componente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
+      <StatefulGreeting />
     </div>
   );
 }
