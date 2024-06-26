82ad229dd78a1891421e8f01f308bef2c496712e
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-26 07:59:56 +0000

    Remove StatefulGreeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
-import StatefulGreeting from './components/StatefulGreeting';
 import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
-      <StatefulGreeting />
       <NavBarSimple />
     </div>
   );
