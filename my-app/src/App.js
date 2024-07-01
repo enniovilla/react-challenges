75ad4db26a156b301b444938309117a9255eb2bd
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-07-01 17:58:35 +0000

    Create API components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import css from "./App.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import ContentHooks from "./components/ContentHooks";
+import ContentAPI from "./components/ContentAPI";
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
       <Sidebar />
-      <ContentHooks />
+      <ContentAPI />
     </div>
   );
 }
